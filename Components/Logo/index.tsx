40159bbb647efd2127c22c44978fa3c1592384f9
artuyees <artuyees@gmail.com>
2022-05-05 19:43:32 +0200

    Updated logo to work correctly with both modes

--- Components/Logo/index.tsx
+++ Components/Logo/index.tsx
@@ -1,20 +1,27 @@
 import { DiamondSharp } from "@mui/icons-material";
 import { Box, Typography } from "@mui/material";
+import Link from "next/link";
 
 const Logo = () => {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-      }}
-    >
-      <DiamondSharp color="secondary" />
-      <Typography variant="h6" color="secondary" component="div" align="center">
-        Earthbnb
-      </Typography>
-    </Box>
+    <Link href="/" passHref>
+      <Box
+        color="secondary.contrastText"
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          "&:hover": {
+            cursor: "pointer",
+          },
+        }}
+      >
+        <DiamondSharp />
+        <Typography variant="h6" component="div" align="center">
+          Earthbnb
+        </Typography>
+      </Box>
+    </Link>
   );
 };
 
